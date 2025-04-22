54c8479ed48f457edd0a6a5704f4b857278a92b0
Zee-RGB <134300176+zee-RGB@users.noreply.github.com>
2025-04-22 18:21:35 +1000

    updated quiz app to loop through questions and ans

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,109 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="ctr">
+    <questions
+      v-if="questionsAnswered < questions.length"
+      :questions="questions"
+    >
+    </questions>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+    <results v-else></results>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <button type="button" class="reset-btn">Reset</button>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import Questions from "./components/Questions.vue";
+import Results from "./components/Results.vue";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  name: "App",
+  components: {
+    Questions,
+    Results,
+  },
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+  data() {
+    return {
+      questionsAnswered: 0,
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+      questions: [
+        {
+          q: "What is 2 + 2?",
+          answers: [
+            {
+              text: "4",
+              is_correct: true,
+            },
+            {
+              text: "3",
+              is_correct: false,
+            },
+            {
+              text: "Fish",
+              is_correct: false,
+            },
+            {
+              text: "5",
+              is_correct: false,
+            },
+          ],
+        },
+        {
+          q: 'How many letters are in the word "Banana"?',
+          answers: [
+            {
+              text: "5",
+              is_correct: false,
+            },
+            {
+              text: "7",
+              is_correct: false,
+            },
+            {
+              text: "6",
+              is_correct: true,
+            },
+            {
+              text: "12",
+              is_correct: false,
+            },
+          ],
+        },
+        {
+          q: "Find the missing letter: C_ke",
+          answers: [
+            {
+              text: "e",
+              is_correct: false,
+            },
+            {
+              text: "a",
+              is_correct: true,
+            },
+            {
+              text: "i",
+              is_correct: false,
+            },
+          ],
+        },
+      ],
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+      results: [
+        {
+          min: 0,
+          max: 2,
+          title: "Try again!",
+          desc: "Do a little more studying and you may succeed!",
+        },
+        {
+          min: 3,
+          max: 3,
+          title: "Wow, you're a genius!",
+          desc: "Studying has definitely paid off for you!",
+        },
+      ],
+    };
+  },
+};
+</script>
